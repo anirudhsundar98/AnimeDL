f16c870a7a3f6659292c52a97540ae9aaceb4c62
Anirudh Sundar <anirudhsundar@hotmail.com>
2018-01-02 11:34:03 +0530

    Added line to rename file after download.

--- lib/episode.rb
+++ lib/episode.rb
@@ -9,13 +9,13 @@ class Episode
   attr_reader :number, :link
 
   def download_to(path = ".")
-    url_base = @link.split('/')[2]
-    url_path = '/'+@link.split('/')[3..-1].join('/')
+    url_base = @link.split('/')[2]                    # Animeheaven
+    url_path = '/'+@link.split('/')[3..-1].join('/')  # Sub Url
 
     Net::HTTP.start(url_base) do |http|
       response = http.request_head(URI.escape(url_path))
       progress_bar = ProgressBar.create(:progress_mark => "\#", :length => 80, :total => response['content-length'].to_i)
-      file = File.new( File.join( path, "Episode #{@number}.mp4" ), 'w')
+      file = File.new( File.join( path, "Episode #{@number}.mp4.tmp" ), 'w')
 
       begin
         http.get(URI.escape(url_path)) do |str|
@@ -23,19 +23,19 @@ class Episode
 
           begin
             progress_bar.progress += str.length
-          rescue
-            # Bypass InvalidProgressBar Error if raised
+          rescue # Bypass InvalidProgressBar Error if raised
           end
         end
       rescue Exception
         # Handle SystemExit or Interrupt
-        puts "Download incomplete. Deleting file."
+        puts "Download incomplete. Deleting file.\n\n"
         File.delete(File.join( path, "Episode #{@number}.mp4" ))
         raise
         exit
       end
 
       progress_bar.finish
+      File.rename(file, File.join( path, "Episode #{@number}.mp4" ))
     end
 
     puts "\n"
